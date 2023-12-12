fd51d90ffa6adc06d0ebb6fbf7e3323a30429406
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2023-12-12 20:31:31 +0000

    Initial commit.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<a href="/"><strong class="text-xl">Fungi-On</strong></a>
+			</svelte:fragment>
+			<input type="search" name="pog" id="heheha" class="button" placeholder="Insert query...">
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/pedro-ramalho/feup-pri-proj"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/species/[speciesId]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let data;
+
+    const result = data.result;
+
+    console.log(result);
+
+</script>
+
+<h3>Viewing page for {result.species}.</h3>
