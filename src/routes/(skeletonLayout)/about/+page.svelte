68cfa310d930e024c4bf00525c7f8e394f171639
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2023-12-14 13:09:56 +0000

    Added basic About

---
+++ src/routes/(skeletonLayout)/about/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="flex flex-row justify-center items-center gap-8">
+	<div class="card variant-filled-primary p-4 flex flex-col items-center gap-2 w-64">
+		<div class="card-header text-xl text-center flex flex-col items-center gap-1">
+			<Avatar
+				src="https://avatars.githubusercontent.com/u/72896498?v=4"
+				width="w-32"
+				rounded="rounded-full"
+			/>
+			<h1>Guilherme Sequeira</h1>
+		</div>
+		<h2 class="text-md text-center">up202004646</h2>
+		<a
+			class="btn btn-sm variant-ghost-surface"
+			href="https://github.com/Guilherme-Soares-Sequeira"
+			target="_blank"
+			rel="noreferrer"
+		>
+			GitHub
+		</a>
+	</div>
+	<div class="card variant-filled-primary p-4 flex flex-col items-center gap-2 w-64">
+		<div class="card-header text-xl text-center flex flex-col items-center gap-1">
+			<Avatar
+				src="https://avatars.githubusercontent.com/u/75331417?v=4"
+				width="w-32"
+				rounded="rounded-full"
+			/>
+			<h1>Pedro Nunes</h1>
+		</div>
+		<h2 class="text-md text-center">up202004714</h2>
+		<a
+			class="btn btn-sm variant-ghost-surface"
+			href="https://github.com/pedronunes19"
+			target="_blank"
+			rel="noreferrer"
+		>
+			GitHub
+		</a>
+	</div>
+	<div class="card variant-filled-primary p-4 flex flex-col items-center gap-2 w-64">
+		<div class="card-header text-xl text-center flex flex-col items-center gap-1">
+			<Avatar
+				src="https://avatars.githubusercontent.com/u/72579386?v=4"
+				width="w-32"
+				rounded="rounded-full"
+			/>
+			<h1>Pedro Ramalho</h1>
+		</div>
+		<h2 class="text-md text-center">up202004715</h2>
+		<a
+			class="btn btn-sm variant-ghost-surface"
+			href="https://github.com/pedro-ramalho"
+			target="_blank"
+			rel="noreferrer"
+		>
+			GitHub
+		</a>
+	</div>
+</div>
